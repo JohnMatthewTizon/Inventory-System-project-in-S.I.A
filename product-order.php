a84d93014972d80af7ff89cfa388ec2adfed95dc
JohnMatthewTizon <21-31773@g.batstate-u.edu.ph>
2023-11-28 12:46:47 +0800

    Add JavaScript Events On Purchase Order Form - onChange onClick Events

--- product-order.php
+++ product-order.php
@@ -31,60 +31,117 @@
           <div class="column">
             <h1 class="section-header">Order Product</h1>
             <div>
-                <div class="alignRight">
-                    <button class="orderBtn orderProductBtn">Order Product</button>
-                </div>
-                <div id="orderProductList"  style="padding-left:30px;">
-                    <div class="orderProductRow">
-                        <div>
-                            <label for="ProductName">Product Name</label>
-                            <select name="ProductName" class="productNameSelect" id="ProductName">
-                                <option value=""></option>
-                            </select>
-                        </div>
+              <div class="alignRight">
+                <button class="orderBtn orderProductBtn" id="orderProductBtn">Add Another Product</button>
+              </div>
 
-                        <div class="suppliersRows">
-                            <div class="row">
-                                <div style="width:50%;">
-                                    <p class="supplierName">Supplier 1</p>
-                                </div>
-                                <div style="width:50%;">
-                                    <label for="quantity">Quantity: </label>
-                                    <input type="number" id="quantity" class="appFormInput" placeholder="Enter Quantity name..." name="quantity" >
-                                </div>
-                                <div style="width:50%;">
-                                    <p class="supplierName">Supplier 2</p>
-                                </div>
-                                <div style="width:50%;">
-                                    <label for="quantity">Quantity: </label>
-                                    <input type="number" id="quantity" class="appFormInput" placeholder="Enter Quantity name...." name="quantity" >
-                                </div>
-                                <div style="width:50%;">
-                                    <p class="supplierName">Supplier 3</p>
-                                </div>
-                                <div style="width:50%;">
-                                    <label for="quantity">Quantity: </label>
-                                    <input type="number" id="quantity" class="appFormInput" placeholder="Enter Quantity name..." name="quantity" >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+              <div id="orderProductLists"  style="padding-left:30px;">
+              </div>
                 
-                <div class="alignRight marginTop20">
-                    <button class="orderBtn submitOrderProductBtn">Submit Order</button>
-                </div>
+              <div class="alignRight marginTop20">
+                <button class="orderBtn submitOrderProductBtn">Submit Order</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+  
   <?php include('partials/app-scripts.php'); ?>
   <script>
     var products = <?= $products ?>;
+    var counter = 0;
 
-    console.log(products);
+    function script() {
+
+      var vm = this;
+
+      let productOptions =  '\
+      <div>\
+        <label for="ProductName">Product Name</label>\
+        <select name="ProductName" class="productNameSelect" id="ProductName">\
+          <option value="">Select Product</option>\
+          INSERTPRODUCTHERE\
+        </select>\
+      </div>';
+      
+
+
+
+
+      this.initialize = function(){
+        this.registerEvents();
+        this.renderProductOptions();
+      },
+
+      this.renderProductOptions = function(){
+        let optionHtml = '';
+        products.forEach((product) => {
+          optionHtml += '<option value="'+ product.id +'">'+ product.ProductName +'</option>';
+        })
+        
+        productOptions = productOptions.replace('INSERTPRODUCTHERE', optionHtml);
+      },
+
+      this.registerEvents = function(){
+        document.addEventListener('click', function(e){
+          targetElement = e.target; // Target element
+          classList = targetElement.classList;
+
+          // Add new product order event
+          if (targetElement.id === 'orderProductBtn') {
+            let orderProductListContainer = document.getElementById('orderProductLists');
+
+            orderProductLists.innerHTML += '\
+                  <div class="orderProductRow">\
+                    '+ productOptions +'\
+                    <div class="suppliersRows" id="supplierRows_'+ counter +'" data-counter="'+ counter +'"></div>\
+                  </div>';
+
+                  counter++;
+          }
+        });
+        
+        document.addEventListener('change', function(e){
+          targetElement = e.target; // Target element
+          classList = targetElement.classList;
+
+          // Add suppliers row on product option change
+          if (classList.contains('productNameSelect')) {
+            let pid = targetElement.value;
+
+            let counterId = targetElement.closest('div.orderProductRow').querySelector('.suppliersRows').dataset.counter;
+
+            $.get('database/get-product-supplier.php', {id: pid} , function(suppliers){
+              vm.renderSupplierRows(suppliers, counterId);
+            }, 'json');
+          }
+        });  
+      },
+
+      this.renderSupplierRows = function(suppliers, counterId) { 
+        let supplierRows = '';
+
+        suppliers.forEach((supplier) => {
+                supplierRows +='\
+                        <div class="row">\
+                          <div style="width:50%;">\
+                            <p class="supplierName">'+ supplier.supplier_name +'</p>\
+                          </div>\
+                          <div style="width:50%;">\
+                            <label for="quantity">Quantity: </label>\
+                            <input type="number" id="quantity" class="appFormInput orderProductQty"  placeholder="Enter Quantity name..." name="quantity" >\
+                          </div>\
+                        </div>';
+        });                        
+
+        //Append to container
+        let supplierRowContainer = document.getElementById('supplierRows_' + counterId);
+        supplierRowContainer.innerHTML = supplierRows;
+      }
+    }
+    
+    (new script()).initialize();
   </script>
   </body>
 </html>
